cd873eebd3f9305cbcea4663d616f5d2bb380557
toondebooser <toondebooser@gmail.com>
2026-01-14 14:40:47 +0100

    Add loader and respons animation on emailjs method

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,35 +1,43 @@
-import { Component, ViewChildren, inject, QueryList, ElementRef } from '@angular/core';
-import { PatternService } from '../services/pattern.service';
-import { EmailService } from '../services/email.service';
+import {
+  Component,
+  ViewChildren,
+  inject,
+  QueryList,
+  ElementRef,
+} from "@angular/core";
+import { PatternService } from "../services/pattern.service";
+import { EmailService } from "../services/email.service";
+import { NgIf, NgClass } from "@angular/common";
 
 @Component({
-  selector: 'app-contact',
+  selector: "app-contact",
   standalone: true,
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  imports: [NgIf, NgClass],
+  templateUrl: "./contact.component.html",
+  styleUrl: "./contact.component.css",
 })
 export class ContactComponent {
- patternService = inject(PatternService);
- emailservice = inject(EmailService);
+  success = false;
+  error = false;
+  loading = false;
+  message = "";
+
+  patternService = inject(PatternService);
+  emailservice = inject(EmailService);
   contactData = this.patternService.contactData;
   validation = this.patternService.validation;
- @ViewChildren("input") inputs!: QueryList<ElementRef>;
+  @ViewChildren("input") inputs!: QueryList<ElementRef>;
 
   getObjectKeys(obj: any): string[] {
     return Object.keys(obj);
   }
-
 
   isValid(input: string, inputType: string): boolean {
     const regexPattern: RegExp = this.patternService.regexPatterns[inputType];
     return regexPattern.test(input);
   }
 
-
-
-  userInput(event: Event, inputType: string): void { 
-    
+  userInput(event: Event, inputType: string): void {
     const input = event.target as HTMLInputElement;
     this.contactData[inputType] = input.value.replaceAll("\n", "<br>");
     input.value === ""
@@ -40,31 +48,40 @@ export class ContactComponent {
       : (this.patternService.valid = false);
   }
 
-  
   sendEmail() {
-     if (Object.values(this.validation).includes(false)) {
+    if (Object.values(this.validation).includes(false)) {
       return;
     }
-    this.emailservice.sendEmail(this.contactData);
-    
+    this.loading = true;
+    this.emailservice
+      .sendEmail(this.contactData)
+      .then((response) => {
+        console.log("Verzonden", response.status, response.text);
+        this.loading = false;
+        this.success = true;
+      })
+      .catch((error) => {
+        console.error("FAILED", error);
+        this.loading = false;
+        this.error = true;
+      });
+
+      // setTimeout()
   }
   checkAnyFieldNotEmpty(obj: any): boolean {
-    return Object.values(obj).some(value => value !== "");
+    return Object.values(obj).some((value) => value !== "");
   }
-  resetMailState(){
-    Object.keys(this.validation).forEach(key => {
+  resetMailState() {
+    Object.keys(this.validation).forEach((key) => {
       this.validation[key] = null;
     });
-    Object.keys(this.contactData).forEach(key => {
-      this.contactData[key] = '';
+    Object.keys(this.contactData).forEach((key) => {
+      this.contactData[key] = "";
     });
-  this.patternService.valid = false;
-  
-
+    this.patternService.valid = false;
   }
-  resetContactSheet(){
-    this.inputs.forEach(input => input.nativeElement.value = '');
+  resetContactSheet() {
+    this.inputs.forEach((input) => (input.nativeElement.value = ""));
     this.resetMailState();
   }
-
 }
--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,16 +1,20 @@
-import emailjs from '@emailjs/browser';
-import { Injectable } from '@angular/core';
+import emailjs from "@emailjs/browser";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class EmailService {
+  private SERVICE_ID = "service_j7krcd8";
+  private TEMPLATE_ID = "template_9m4n4ea";
+  private PUBLIC_KEY = "nD8c0oopLsuhHOiPT";
 
-  private SERVICE_ID = 'service_j7krcd8';
-  private TEMPLATE_ID = 'template_9m4n4ea';
-  private PUBLIC_KEY = 'nD8c0oopLsuhHOiPT';
-
-  sendEmail(contactData: { email: string; bericht: string; onderwerp: string; telefoon: Number; }) {
+  sendEmail(contactData: {
+    email: string;
+    bericht: string;
+    onderwerp: string;
+    telefoon: Number;
+  }) {
     return emailjs.send(
       this.SERVICE_ID,
       this.TEMPLATE_ID,
@@ -18,7 +22,7 @@ export class EmailService {
         email: contactData.email,
         bericht: contactData.bericht,
         onderwerp: contactData.onderwerp,
-        telefoon: contactData.telefoon
+        telefoon: contactData.telefoon,
       },
       this.PUBLIC_KEY
     );
