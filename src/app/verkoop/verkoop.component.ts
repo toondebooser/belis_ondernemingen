6830d1209d2c7f6e81d0282c579121ed5cbad5c9
toondebooser <toondebooser@gmail.com>
2026-01-20 13:14:11 +0100

    Add client site content

--- src/app/verkoop/verkoop.component.ts
+++ src/app/verkoop/verkoop.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-verkoop',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './verkoop.component.html',
   styleUrl: './verkoop.component.css'
 })
